db08d885dba408c9e671c107104e855855e0f9a6
bltnico <bellot.n@gmail.com>
2019-02-24 18:05:57 +0100

    Add scroll view demo app

---
+++ components/ScrollViewDemo.js
@@ -0,0 +1,89 @@
+import React, { PureComponent } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  ScrollView,
+} from 'react-native';
+
+const { width: vw, height: vh } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
+  },
+  title: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  item: {
+    width: vw,
+    height: vh / 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    width: 100, 
+    height: 100,
+    marginLeft: 20,
+  },
+});
+
+const getRandomColor = () => '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+
+const items = [
+  { color: getRandomColor(), img: 'https://picsum.photos/100?random' },
+  { color: getRandomColor(), img: 'https://picsum.photos/100?random' },
+  { color: getRandomColor(), img: 'https://picsum.photos/100?random' },
+  { color: getRandomColor(), img: 'https://picsum.photos/100?random' },
+  { color: getRandomColor(), img: 'https://picsum.photos/100?random' },  
+];
+
+const MAIN_TITLE = 'React Native demo app';
+
+class ScrollViewDemo extends PureComponent {
+
+  _renderItems = () => {
+    return items.map((item, index) => {
+      const itemIndex = index + 1;
+      return (
+        <TouchableOpacity
+          key={index}
+          activeOpacity={0.9}
+          style={[styles.item,{ backgroundColor: item.color }]}
+          onPress={() => alert(itemIndex)}>
+          <View style={styles.itemWrapper}>
+            <Text>Item numéro: {itemIndex}</Text>
+            <Image 
+              style={styles.img}
+              source={{ uri: item.img }} />
+          </View>
+        </TouchableOpacity>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>{MAIN_TITLE}</Text>
+        {this._renderItems()}
+      </ScrollView>
+    );
+  }
+}
+
+export default ScrollViewDemo;
